8b331e36d1a9271df8c8dc922f2c3113cb19ea02
Jasmine Chen <j.lin013@gmail.com>
2016-06-09 05:39:09 +0800

    Update tester

--- tester.cpp
+++ tester.cpp
@@ -1,16 +1,24 @@
 #include "vector.h"
-
-using lni::vector;
+#include <vector>
+#include <ctime>
 
 int main() {
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.emplace_back(4);
-	v.emplace_back(5);
-	v.emplace_back(6);
-	for (auto &n: v)
-		printf("%d\n", n);
+	int i;
+	time_t st;
+
+	st = clock();
+	lni::vector<int> v1;
+	for (i = 0; i < 10000000; ++i)
+		v1.push_back(i);
+	printf("lni::vector %.3fs\n", (double)(clock() - st) / CLOCKS_PER_SEC);
+//	for (auto &n: v1)
+//	 	printf("%d ", n);
+	
+	st = clock();
+	std::vector<int> v2;
+	for (i = 0; i < 10000000; ++i)
+		v2.push_back(i);
+	printf("std::vector %.3fs\n", (double)(clock() - st) / CLOCKS_PER_SEC);
+
 	return 0;
 }
