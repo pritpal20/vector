1bdd876615deb6e6f952c59f025d99eaad5f4d28
Jasmine Chen <j.lin013@gmail.com>
2016-06-25 20:18:26 +0800

    Add test script

---
+++ bench/test.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+if [ -z $1 ]; then
+	echo "* Please select a bench to test!"
+	exit
+elif [ ! -e $1.cpp ]; then
+	echo "* Bench doesn't exist!"
+	exit
+fi
+
+printf "* Building ..."
+make -s std_$1
+make -s lni_$1
+echo "Done"
+echo ""
+
+echo "* std::vector"
+./std_$1
+echo ""
+
+echo "* lni::vector"
+./lni_$1
