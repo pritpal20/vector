197bd2c7014c2eb158447c3b7171d70bb879fcf7
Jasmine Chen <j.lin013@gmail.com>
2016-06-29 21:10:12 +0800

    Add folly install script

---
+++ install_folly.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sudo apt-get install \
+    automake \
+    autoconf \
+    autoconf-archive \
+    libtool \
+    libboost-all-dev \
+    libevent-dev \
+    libdouble-conversion-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libsnappy-dev \
+    make \
+    zlib1g-dev \
+    binutils-dev \
+    libjemalloc-dev \
+    libssl-dev \
+    libiberty-dev
+
+cd ~
+git clone https://github.com/facebook/folly.git
+cd folly; cd folly
+
+autoreconf -ivf
+./configure
+make
+make check
+sudo make install
